b36ba2aa09c29f41ec90ab055447b3507dfa8811
yscpeak <88088271+yscpeak@users.noreply.github.com>
2023-09-26 05:15:19 -0500

    Update Polymorphism.java

--- Java/Polymorphism.java
+++ Java/Polymorphism.java
@@ -1,14 +1,17 @@
 public class PolyProto {
-    public static void main(String[] args) { // program entry point
-        Animal myAnimal = new Dog(); // Polymorphism
+    public static void main(String[] args) { // Program entry point
+        
+        // (Polymorphism) Create an instance of Dog. Assign it to an Animal type of a variable.  
+        Animal myAnimal = new Dog(); 
+        
         myAnimal.makeSound();
 
-        myAnimal = new Cat(); // Polymorphism
-        myAnimal.makeSound();
+        myAnimal = new Cat(); // (Polymorphism) Points myAnimal to a new instance of Cat. 
+        myAnimal.makeSound(); // Since myAnimal points to Cat, calling makeSound() will print Meow!.
     }
 }
 
-public class Animal {
+public class Animal { // Parent class = Base class
     void makeSound() {
         System.out.println("Some sound");
     }
